985f3c4142a6d50355f0d5dfa9b702a4a0bc4160
Andrei Hodorog <hodorog.andrei@gmail.com>
2017-02-22 22:23:55 +0200

    * got search by keyword working

--- src/main/java/com/xpressstarter/repository/CampaignRepository.java
+++ src/main/java/com/xpressstarter/repository/CampaignRepository.java
@@ -3,14 +3,16 @@ package com.xpressstarter.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.xpressstarter.entity.Campaign;
 import com.xpressstarter.util.CampaignCategory;
 
 public interface CampaignRepository extends PagingAndSortingRepository<Campaign, String> {
 
-	public Page<Campaign> findByBeneficiaryId(String userId,Pageable Page);
-	public Page<Campaign> findByCategory(CampaignCategory category, Pageable Page);
-	public Page<Campaign> findByNameLikeOrDescriptionLikeAllIgnoreCase(String keyword, Pageable Page);
+	public Page<Campaign> findByBeneficiaryId(@Param("userId")String userId,Pageable Page);
+	public Page<Campaign> findByCategory(@Param("category")CampaignCategory category, Pageable Page);
+	public Page<Campaign> findByNameContainingOrDescriptionContainingAllIgnoreCase(@Param("keyword") String name,@Param("keyword") String description , Pageable Page);
+	
 	public Campaign findByName(String name);
 }
