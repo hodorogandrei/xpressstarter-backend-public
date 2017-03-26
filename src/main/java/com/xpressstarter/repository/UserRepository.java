434d050bbc16b770f1dcd1e8853d7d7c1bef53b6
Andrei Hodorog <hodorog.andrei@gmail.com>
2017-03-26 22:31:20 +0300

    + added a new way of searching for users based on their roles

--- src/main/java/com/xpressstarter/repository/UserRepository.java
+++ src/main/java/com/xpressstarter/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.xpressstarter.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.xpressstarter.entity.User;
+import com.xpressstarter.util.Role;
 
 public interface UserRepository extends MongoRepository<User, String>{
 
 	public User findByEmail(@Param("email")String email);
+	public List<User> findByRole(Role role);
 }
